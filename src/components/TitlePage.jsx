a20a66aa717d0f870cf91edc580c52755267f03e
Caleb Campbell <calebcampbellcrm@gmail.com>
2022-12-13 19:29:20 -0700

    wip

--- src/components/TitlePage.jsx
+++ src/components/TitlePage.jsx
@@ -9,7 +9,9 @@ export default function TitlePage() {
         <h3>and I am a</h3>
         <h2>Full Stack Web Developer.</h2>
         <p> I am a developer with a passion for creating real-world applications to help real-world organizations. I'm a fast, effiecient learner with a no-nonsense mentality. I am most familiar with JavaScript, React, ExpressJS, and SQL, but I am always eager to learn new skills, especially when they meet YOUR needs. Lets get in touch!</p>
+        <div className='title-btn-wrap'>
         <button className='contact-me'>Contact Me</button>
+        </div>
     </div>
   )
 }
